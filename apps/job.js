ecbc01a1fb638a051be0f6d098e1d7c6b8c291db
LisonEvf <LisonEvf@gmail.com>
2023-09-12 18:08:10 +0800

    工程清理

--- apps/job.js
+++ apps/job.js
@@ -1,22 +1,46 @@
-﻿import { App } from '../components/index.js'
+﻿import { App, Permission } from '../components/index.js'
 
 export default new App({
   name: 'job',
   desc: '任务'
 }).config({
-  joblist: {
-    reg: /^\/\/joblist$/,
-    check: (e) => {
-      return e.isMaster
-    },
-    func: (e) => {
+  jobList: {
+    command: '//任务列表 //任务',
+    desc: '查询任务列表',
+
+    reg: /^\/\/任务(列表)?$/,
+    func: e => {
         e.reply('you are master')
     }
   },
 
-  addjob: {
-    reg: /^\/\/addjob$/,
-    func: (e) => {
+  addJob: {
+    command: '//添加任务 //增加任务 //新增任务',
+    desc: '添加任务',
+
+    reg: /^\/\/(添加|增加|新增)任务$/,
+    permission: Permission.admin,
+    func: e => {
+      e.reply(`your QQNumber is ${e.user_id}`)
+    }
+  },
+
+  delJob: {
+    command: '//删除任务 //移除任务',
+    desc: '删除任务',
+
+    reg: /^\/\/(删除|移除)任务$/,
+    func: e => {
+      e.reply(`your QQNumber is ${e.user_id}`)
+    }
+  },
+
+  updateJob: {
+    command: '//修改任务',
+    desc: '修改任务',
+
+    reg: /^\/\/修改任务$/,
+    func: e => {
       e.reply(`your QQNumber is ${e.user_id}`)
     }
   }
--- apps/server.js
+++ apps/server.js
@@ -4,16 +4,57 @@ export default new App({
   name: 'server',
   desc: '服务器'
 }).config({
-  addserver: {
-    reg: /^\/\/添加服务器$/,
+  serverList: {
+    command: '//服务器 //服务器列表 //查询服务器 //查询服务器列表',
+    desc: '查询服务器列表',
+
+    reg: /^\/\/(查询)?服务器(列表)?$/,
     permission: Permission.admin,
-    func: (e) => {
+    func: e => {
+      //
+    }
+  },
+
+  addServer: {
+    command: '//添加服务器 //添加服务器配置 //新增服务器 //新增服务器配置',
+    desc: '添加服务器',
+
+    reg: /^\/\/(添加|增加|新增)服务器(配置)?$/,
+    permission: Permission.admin,
+    func: e => {
         e.reply('you are master')
     }
   },
-  delserver: {
-    reg: /^\/\/删除服务器$/,
-    func: (e) => {
+
+  delServer: {
+    command: '//删除服务器 //删除服务器配置',
+    desc: '删除服务器',
+
+    reg: /^\/\/删除服务器(配置)?$/,
+    permission: Permission.admin,
+    func: e => {
+      e.reply(`your QQNumber is ${e.user_id}`)
+    }
+  },
+
+  updateServer: {
+    command: '//修改服务器 //修改服务器配置',
+    desc: '修改服务器',
+
+    reg: /^\/\/修改服务器(配置)?$/,
+    permission: Permission.admin,
+    func: e => {
+      e.reply(`your QQNumber is ${e.user_id}`)
+    }
+  },
+
+  bindServer: {
+    command: '//绑定服务器',
+    desc: '绑定服务器',
+
+    reg: /^\/\/绑定服务器$/,
+    permission: Permission.admin,
+    func: e => {
       e.reply(`your QQNumber is ${e.user_id}`)
     }
   }
